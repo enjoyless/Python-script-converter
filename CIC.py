a79b1ebe26c1750fb60c658a4195a76b43a64238
ZYunH <workvl@163.com>
2016-05-28 11:39:10 +0800

    ver1.1

--- CIC.py
+++ CIC.py
@@ -1,11 +1,28 @@
 # -*- coding:utf-8 -*-
-from tools import *
 import os
-import sys
 
-file_name = sys.argv[0]
 
-content = text_read(file_name)
+def text_read(filename):
+    try:
+        file = open(filename, 'r')
+    except IOError:
+        error = []
+        return error
+    content = file.readlines()
+
+    for i in range(len(content)):
+        if content[i][len(content[i]) - 1] == '\n':
+            content[i] = content[i][:len(content[i]) - 1]
+
+    file.close()
+    return content
+
+
+def text_save(content, filename, mode='a'):
+    file = open(filename, mode)
+    for i in range(len(content)):
+        file.write(str(content[i]) + '\n')
+    file.close()
 
 
 def add(content, file_name):
@@ -21,13 +38,13 @@ def add(content, file_name):
             import_sys_key = 0
 
     for i in content:
-        if i.find('import')==0 or i.find('#')==0:
+        if i.find('import') == 0 or i.find('#') == 0:
             pass
         elif import_os_key:
-            content.insert(content.index(i) , 'import os')
+            content.insert(content.index(i), 'import os')
             import_os_key = 0
         elif import_sys_key:
-            content.insert(content.index(i) , 'import sys')
+            content.insert(content.index(i), 'import sys')
             import_sys_key = 0
 
     # Add header
@@ -35,13 +52,17 @@ def add(content, file_name):
         content.insert(0, '#!/usr/bin/env python')
 
     # Add path
-    if content[len(content)-2] != '# Prevent path error.By ZYunH.':
+    for i in content:
+        if i.find('import') == 0 or i.find('#') == 0:
+            pass
+        else:
+            list_path = ['# Prevent path error.By ZYunH.', 'CIC_path = os.path.dirname(sys.argv[0])', 'os.chdir(CIC_path)']
 
-        list_path = ['# Prevent path error.By ZYunH.', '_path = os.path.dirname(sys.argv[0])', 'os.chdir(_path)']
+            for j in range(len(list_path)):
+                content.insert(content.index(i), list_path[j])
+            break
 
-        content.insert(len(content),'')
-        for i in range(len(list_path)):
-            content.insert(len(content), list_path[i])
+    print content[len(content) - 5]
 
     text_save(content, file_name[:-3] + '_temp' + '.py', mode='w')
 
@@ -53,6 +74,10 @@ def change(name):
 
     os.system('chmod +x %s' % name_change)
 
+
+file_name = 'newmd_english.py'
+
+content = text_read(file_name)
 
 add(content=content, file_name=file_name)
 
